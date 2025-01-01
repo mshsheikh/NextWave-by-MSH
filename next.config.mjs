aa3d31b4245b03a6d0c52d96dbed00ee3acac8d4
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-01-01 08:31:09 +0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["preview.colorlib.com", "images.unsplash.com", "plus.unsplash.com"], // Multiple hostnames
-  },
-};
-
-export default nextConfig;
