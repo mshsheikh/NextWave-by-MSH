5f0fb1c9384dcdfd134db7d21712827eb20ca70c
MUHAMMAD SALMAN HUSSAIN <160324527+mshsheikh@users.noreply.github.com>
2025-01-01 05:58:33 +0500

    first commit

---
+++ Data/displayData.ts
@@ -0,0 +1,80 @@
+export type PropsDisplay = {
+  title: string;
+  category: string;
+  date: string;
+  image: string;
+};
+
+export const displayData: PropsDisplay[] = [
+  {
+    title: "Journey of Quantum Computing",
+    category: "Tech",
+    date: "September 16, 2024",
+    image: "/quantum-computing.png",
+  },
+
+  {
+    title: "Prioritize Mental Health at Workplace",
+    category: "Health",
+    date: "April 15, 2024",
+    image: "/mental-health-in-the-workplace.png",
+  },
+
+  {
+    title: "The Gig Economy and Financial Stability",
+    category: "Finance",
+    date: "June 28, 2024",
+    image: "/gig-economy.png",
+  },
+
+  {
+    title: "Impact of Animal Migration Patterns",
+    category: "Animals",
+    date: "November 19, 2024",
+    image: "/migration.png",
+  },
+];
+
+export const displayData2: PropsDisplay[] = [
+  {
+    title: "Journey of Quantum Computing",
+    category: "Tech",
+    date: "September 16, 2024",
+    image: "/quantum-computing.png",
+  },
+
+  {
+    title: "Prioritize Mental Health at Workplace",
+    category: "Health",
+    date: "April 15, 2024",
+    image: "/mental-health-in-the-workplace.png",
+  },
+
+  {
+    title: "The Gig Economy and Financial Stability",
+    category: "Finance",
+    date: "June 28, 2024",
+    image: "/gig-economy.png",
+  },
+
+  {
+    title: "Impact of Animal Migration Patterns",
+    category: "Animals",
+    date: "November 19, 2024",
+    image: "/migration.png",
+  },
+
+  {
+    title: "How 5G is Transforming Smart Cities and IoT",
+    category: "Tech",
+    date: "August 31, 2024",
+    image: "/5G.png",
+  },
+
+  {
+    title: "The Importance of Gut Health for Your Overall Wellness",
+    category: "Health",
+    date: "February 2, 2024",
+    image: "/gut-health.png",
+  },
+];
